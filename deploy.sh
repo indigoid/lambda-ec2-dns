78817bd46d505ac247330cb606eb470603564b10
John Slee <john.slee@fairfaxmedia.com.au>
2016-11-12 01:44:50 +1100

    deployer niceties

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,8 @@ npm install
 zip -r ${HOME}/dist.zip .
 cd
 
-# the alias used on lambda is production|develop, but the branches we have are master|develop
+# the alias used on lambda is production|develop, but the branches we
+# have are master|develop
 case $CI_BRANCH in
   *develop*)
     ALIAS="develop"
@@ -34,9 +35,9 @@ case $CI_BRANCH in
     ;;
 esac
 
-# Preparing and deploying Function to Lambda
+# Preparing and deploying the function to Lambda
 aws lambda update-function-code --function-name $LAMBDA_FUNCTION --zip-file "fileb://${HOME}/dist.zip"
-# Publishing a new Version of the Lambda function
+# Publishing a new version of the Lambda function
 version=`aws lambda publish-version --function-name $LAMBDA_FUNCTION | jq -r .Version`
-# Updating the PROD Lambda Alias so it points to the new function
+# Updating the production Lambda Alias so it points to the new function
 aws lambda update-alias --function-name $LAMBDA_FUNCTION --function-version "${version}" --name $ALIAS --description "${CI_COMMIT_ID} - ${CI_MESSAGE:0:212}"
